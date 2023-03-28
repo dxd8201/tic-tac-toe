93d4969a73dd6395a7c6539d9301468d73be2410
dxd8201 <ddronet17@gmail.com>
2023-03-28 16:02:16 -0500

    added winCheck function that determines a win

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gameBoard = (() => {
 
     const renderGameBoard = function () {
         for (i=0; i<9; i++) {
-            board[i] = i;
+            board[i] = 0;
             const gameBoardSquare = document.createElement("div");
             gameBoardElt.appendChild(gameBoardSquare);
             gameBoardSquare.classList.add("game-board-square");
@@ -38,15 +38,48 @@ const gameController = (() => {
     playerOne.introduction();
     playerTwo.introduction();
 
+    
+
+    const winCheck = () => {
+        if (
+            (gameBoard.board[0] === 1 && gameBoard.board[1] === 1 && gameBoard.board[2] === 1) ||
+            (gameBoard.board[3] === 1 && gameBoard.board[4] === 1 && gameBoard.board[5] === 1) ||
+            (gameBoard.board[6] === 1 && gameBoard.board[7] === 1 && gameBoard.board[8] === 1) ||
+            (gameBoard.board[0] === 1 && gameBoard.board[3] === 1 && gameBoard.board[6] === 1) ||
+            (gameBoard.board[1] === 1 && gameBoard.board[4] === 1 && gameBoard.board[7] === 1) ||
+            (gameBoard.board[2] === 1 && gameBoard.board[5] === 1 && gameBoard.board[8] === 1) ||
+            (gameBoard.board[0] === 1 && gameBoard.board[4] === 1 && gameBoard.board[8] === 1)
+            ) {
+            console.log("Player one wins!");
+        } else if(
+            (gameBoard.board[0] === 2 && gameBoard.board[1] === 2 && gameBoard.board[2] === 2) ||
+            (gameBoard.board[3] === 2 && gameBoard.board[4] === 2 && gameBoard.board[5] === 2) ||
+            (gameBoard.board[6] === 2 && gameBoard.board[7] === 2 && gameBoard.board[8] === 2) ||
+            (gameBoard.board[0] === 2 && gameBoard.board[3] === 2 && gameBoard.board[6] === 2) ||
+            (gameBoard.board[1] === 2 && gameBoard.board[4] === 2 && gameBoard.board[7] === 2) ||
+            (gameBoard.board[2] === 2 && gameBoard.board[5] === 2 && gameBoard.board[8] === 2) ||
+            (gameBoard.board[0] === 2 && gameBoard.board[4] === 2 && gameBoard.board[8] === 2)
+        ) {
+            console.log("Player two wins!");
+        } else {
+            console.log("No one has won yet!")
+        }
+    }   
+
     for(i=0; i<9; i++) {
         let gameBoardSquareListener = document.querySelectorAll("#gameBoardSquare");
             gameBoardSquareListener[i].addEventListener('click', function(i) {
                 if(gameController.currentPlayer === playerOne) {
                     gameBoard.gameBoardSquare[i].textContent = "X";
+                    gameBoard.board[i] = 1;
+                    console.log(gameBoard.board);
                     gameController.currentPlayer = playerTwo;
+                    winCheck();
                 } else if (gameController.currentPlayer === playerTwo) {
                     gameBoard.gameBoardSquare[i].textContent = "O";
+                    gameBoard.board[i] = 2;
                     gameController.currentPlayer = playerOne;
+                    winCheck();
                 }
                 console.log(`you clicked Game Board Square ${i}`);
                 this.onclick = null;
