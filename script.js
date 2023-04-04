33323eaf8d769b5d4885b6b4a19f90bb718fd989
dxd8201 <ddronet17@gmail.com>
2023-04-03 21:08:06 -0500

    Wired up event listeners for score display Wired up event listeners for current player To do: wire up reset board button

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 //module for generating the game board
 const gameBoard = (() => {
 
+    const resetBoardBtn = document.getElementById("resetBtn");
     const gameBoardElt = document.getElementById("gameBoardContainer");
-    const board = [];
+    let board = [];
 
     const renderGameBoard = function () {
         for (i=0; i<9; i++) {
@@ -16,20 +17,28 @@ const gameBoard = (() => {
             gameBoardSquare.setAttribute("id", "gameBoardSquare");
             console.log("You rendered a game square");
         }
+        
     }
 
     const resetBoard = function () {
         board = [];
+        // renderGameBoard();
+        console.log("You reset the board!");
     }
 
+    
+    resetBoardBtn.addEventListener("click", resetBoard());
     renderGameBoard();
     console.log(board)
+
     return {renderGameBoard, gameBoardSquare, board, resetBoard}
 })();
 
 //controls players and states of the game
 const gameController = (() => {
     let victory = false;
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
     
     //function factory for generating players
     const playerFactory = (player) => {
@@ -54,7 +63,9 @@ const gameController = (() => {
             (gameBoard.board[2] === 1 && gameBoard.board[5] === 1 && gameBoard.board[8] === 1) ||
             (gameBoard.board[0] === 1 && gameBoard.board[4] === 1 && gameBoard.board[8] === 1)
             ) {
-            victory = true
+            victory = true;
+            gameController.playerOneScore += 1;
+            displayController.scoreDisplay();
             console.log("Player one wins!");
         } else if(
             (gameBoard.board[0] === 2 && gameBoard.board[1] === 2 && gameBoard.board[2] === 2) ||
@@ -66,6 +77,8 @@ const gameController = (() => {
             (gameBoard.board[0] === 2 && gameBoard.board[4] === 2 && gameBoard.board[8] === 2)
         ) {
             victory = true;
+            gameController.playerTwoScore += 1;
+            displayController.scoreDisplay();
             console.log("Player two wins!");
         } else {
             console.log("No one has won yet!")
@@ -82,11 +95,13 @@ const gameController = (() => {
                     gameBoard.board[i] = 1;
                     console.log(gameBoard.board);
                     gameController.currentPlayer = playerTwo;
+                    displayController.turnTracker();
                     winCheck();
                 } else if (gameController.currentPlayer === playerTwo && victory === false) {
                     gameBoard.gameBoardSquare[i].textContent = "O";
                     gameBoard.board[i] = 2;
                     gameController.currentPlayer = playerOne;
+                    displayController.turnTracker();
                     winCheck();
                 } 
                 console.log(`you clicked Game Board Square ${i}`);
@@ -94,10 +109,30 @@ const gameController = (() => {
                 }.bind(null, i), {once: true});
     } 
 
-    return { currentPlayer, playerOne, playerTwo }
+    return { currentPlayer, playerOne, playerTwo, playerOneScore, playerTwoScore, victory }
 })();
 
 //module for generating displays
 const displayController = (() => {
+    const p1Score = document.getElementById("p1Score");
+    const p2Score = document.getElementById("p2Score");
+    const currentPlayer = document.getElementById("currentPlayerValue");
 
+    const turnTracker = function () {
+        if(gameController.currentPlayer === gameController.playerOne) {
+            currentPlayer.textContent = "Player 1";
+        } else if(gameController.currentPlayer === gameController.playerTwo) {
+            currentPlayer.textContent = "Player 2";
+        }
+    }
+    
+    const scoreDisplay = function () {
+        p1Score.textContent = gameController.playerOneScore;
+        p2Score.textContent = gameController.playerTwoScore;
+    }
+
+    scoreDisplay();
+    currentPlayer.textContent = gameController.currentPlayer.player;
+
+    return {turnTracker, scoreDisplay}
 })();
